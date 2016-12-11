5dfe75824d50be572c2eb9edc0da8c6003614916
RBasic <rcham.pro@gmail.com>
2016-12-11 10:36:44 +0100

    first push

---
+++ Assets/Touched.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touched : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void touch()
+    {
+
+        Debug.Log("touched");
+    }
+}
